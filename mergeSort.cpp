9ff9e301ecb68ed7b2af8ca8dd4b3c905cdd5c82
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-17 11:28:40 +0700

    second commit

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -3,3 +3,15 @@ using namespace std;
 
 int arr[20], B[];   // Ukuran array maksimum 20
 int n;           // Variabel untuk panjang array
+void input() {
+    while (true) {
+        cout << "Masukkan panjang elemen array: ";
+        cin >> n;
+
+        if (n <= 20) {
+            break;
+        } else {
+            cout << "\nMaksimal panjang array adalah 20\n";
+        }
+    }
+}
