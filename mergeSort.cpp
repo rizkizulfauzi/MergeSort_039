257827dbd2bfd2aac49eba5ee2c1e4067d1b3a9b
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-17 11:32:42 +0700

    step 4.a

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -30,4 +30,8 @@ void mergeSort(int low, int high) {
     int mid = (low + high) / 2;
     mergeSort(low, mid);
     mergeSort(mid + 1, high);  
+
+    int i = low;
+    int j = mid + 1;
+    int k = low;
 }
