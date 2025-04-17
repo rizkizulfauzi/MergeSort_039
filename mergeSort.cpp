5452006156b781bf087247c479278d412be05f82
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-04-17 11:33:56 +0700

    commit 9

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -44,5 +44,19 @@ void mergeSort(int low, int high) {
         }
         k++;
     }
+    while(j <= high) {
+        B[k] = arr[j];
+        j++;
+        k++;
+    }
+    while(i <= mid) {
+        B[k] = arr[i];
+        i++;
+        k++;
+    }
 
+    for (int x = low; x <= high; x++) {
+        arr[x] = B[x];
+    }
 }
+
